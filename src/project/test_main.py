e7d9b2a6a7ab2270185c017c942ed39c2d84f4b2
maurapintor <maura.pintor@gmail.com>
2024-03-15 14:20:03 +0100

    test automation

---
+++ src/project/test_main.py
@@ -0,0 +1,11 @@
+
+
+from functions.main_functions import f
+
+
+def test_function_string():
+    assert f("a") == "a", "Should be a"
+
+def test_function_number():
+    assert f(1) == 1, "Should be 1"
+
