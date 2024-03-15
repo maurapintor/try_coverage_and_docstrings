0caa0198dad6f9b7cb03b25fbc055dd95fb14b9b
maurapintor <maura.pintor@gmail.com>
2024-03-15 18:46:06 +0100

    added src path

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+
+
+from project.functions.main_functions import f
+
+
+def test_function_string():
+    assert f("a") == "a", "Should be a"
+
+def test_function_number():
+    assert f(1) == 1, "Should be 1"
+
+ 
