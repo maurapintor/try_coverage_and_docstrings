a9ce45b3bf145ea4f16b8ce38148cac185d5df9e
maurapintor <maura.pintor@gmail.com>
2024-03-16 12:28:52 +0100

    add docs checks

--- src/project/functions/another_function.py
+++ src/project/functions/another_function.py
@@ -1,2 +1,17 @@
-def p(x):
+"""Returns float numbers."""
+
+
+def p(x: int) -> float:
+    """Return the input as a float.
+
+    Parameters
+    ----------
+    x : int
+        an integer
+
+    Returns
+    -------
+    float
+        the input as float
+    """
     return float(x)
