8e4d7d91cf9359e6377b7677076d530ca5d33c2a
maurapintor <maura.pintor@gmail.com>
2024-03-16 18:06:46 +0100

    fix test

--- tests/test_another_function.py
+++ tests/test_another_function.py
@@ -2,8 +2,8 @@ from project.functions.another_function import another, p
 
 
 def test_p_function() -> None:
-    assert p(3) == float(3), "Should be 3"
+    assert p(x=3) == float(3), "Should be 3"
 
 
 def test_another_function() -> None:
-    assert another(3) == "False", "Should be False"
+    assert another(x="a") is True, "Should be False"
