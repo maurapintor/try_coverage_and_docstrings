0c208efbb7c2051617f62d0f36bbefd756ed260d
maurapintor <maura.pintor@gmail.com>
2024-03-16 13:58:37 +0100

    added docstrings

--- src/project/functions/main_functions.py
+++ src/project/functions/main_functions.py
@@ -1,10 +1,52 @@
+"""Main functions."""
+
+
 def f(a):
+    """
+    Return the same value.
+
+    Parameters
+    ----------
+    a : any
+        Anything.
+
+    Returns
+    -------
+    any
+        The same as the input a.
+    """
     return a
 
 
 def g(b):
+    """
+    Convert to string.
+
+    Parameters
+    ----------
+    b : any
+        Any input that will be returned as string.
+
+    Returns
+    -------
+    any
+        The input b as a string.
+    """
     return str(b)
 
 
 def func_l(x):
+    """
+    Check if the input is a string.
+
+    Parameters
+    ----------
+    x : any
+        Any object.
+
+    Returns
+    -------
+    bool
+        True if the input is a string.
+    """
     return isinstance(x, str)
--- src/project/utils/calculator.py
+++ src/project/utils/calculator.py
@@ -1,6 +1,39 @@
+"""Calculator functions."""
+
+
 def calc_sum(a, b):
+    """
+    Sum two elements.
+
+    Parameters
+    ----------
+    a : number
+        A number.
+    b : number
+        Another number.
+
+    Returns
+    -------
+    number
+        The sum of the two inputs.
+    """
     return a + b
 
 
 def calc_mul(a, b):
+    """
+    Multiply two elements.
+
+    Parameters
+    ----------
+    a : number
+        A number.
+    b : number
+        Another number.
+
+    Returns
+    -------
+    number
+        The product of the two inputs.
+    """
     return a * b
